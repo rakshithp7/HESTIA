84d808c52c589750dfe04dfd7ac7e9de91b90409
Rakshith Poojary <rakshithp7@gmail.com>
2025-10-08 19:05:22 -0400

    update 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,18 +3,26 @@
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import { useTheme } from '@/components/theme-provider';
 
 export default function NotFound() {
+  const { theme } = useTheme();
+
+  const animationSrc =
+    theme === 'dark'
+      ? 'https://lottie.host/6b15ec54-ed7f-4403-b465-ce2446497b19/xaCN3TDHzb.lottie'
+      : 'https://lottie.host/69d9dd64-a9bd-40fe-b9ac-0e359eb4f438/GsrMFFEmZh.lottie';
+
   return (
     <div className="flex flex-col items-center justify-center px-6 py-16 text-center">
       <div className="relative flex flex-col items-center gap-10">
-        <DotLottieReact src="/404-anim.lottie" loop autoplay className="w-3/4" />
+        <DotLottieReact src={animationSrc} loop autoplay className="w-[100vw] md:w-[50vw]" />
 
         <div className="space-y-4 max-w-xl">
           <h1 className="text-3xl md:text-4xl font-semibold">Well, this is awkward.</h1>
           <p className="text-lg">
-            He&apos;s stuck trying to get the yarn, and you&apos;re stuck on this page. One of these problems is easier
-            to solve. Let&apos;s start with yours.
+            Our cat&apos;s stuck trying to get the yarn, and you&apos;re stuck on this page. One of these problems is
+            easier to solve. Let&apos;s start with yours.
           </p>
         </div>
 
